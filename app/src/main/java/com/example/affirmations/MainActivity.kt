dbcde66fa600f5b47d2999e2c6910effa1fd2a5d
Maria Basia <maria.basia1@gmail.com>
2024-07-24 14:42:46 +0100

    affirmation card list created, design needs to be fixed

--- app/src/main/java/com/example/affirmations/MainActivity.kt
+++ app/src/main/java/com/example/affirmations/MainActivity.kt
@@ -18,31 +18,58 @@ package com.example.affirmations
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.Surface
+import androidx.compose.material3.Card
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import com.example.affirmations.ui.theme.AffirmationsTheme
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.tooling.preview.Preview
+import com.example.affirmations.data.Datasource
+import com.example.affirmations.model.Affirmation
 
 class MainActivity : ComponentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            AffirmationsTheme {
-                // A surface container using the 'background' color from the theme
-                Surface(
-                    modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
-                ) {
-                    AffirmationsApp()
-                }
-            }
+            AffirmationsApp()
         }
     }
 }
 
+@Preview
 @Composable
 fun AffirmationsApp() {
+    val datasource = Datasource()
+    val affirmationsArray = datasource.loadAffirmations()
+    
+    Column {
+        for (item in affirmationsArray) {
+            AffirmationCard(
+                affirmation = item,
+                modifier = Modifier
+                )
+        }
+    }
+
+
+}
+
+
+
+
+@Composable
+fun AffirmationCard(
+    affirmation: Affirmation,
+    modifier: Any = Modifier
+        .fillMaxSize()
+) {
+    Card {
+        Image(painter = painterResource(id = affirmation.imageResourceID), contentDescription = "" )
+        Text(text = stringResource(id = affirmation.stringResourceID))
+    }
 }
